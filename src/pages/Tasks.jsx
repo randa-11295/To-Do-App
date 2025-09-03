a57bae613cf2565e344695233e1a7218b446924d
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-09-04 00:00:08 +0300

    add pop up

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -4,54 +4,62 @@ import Paper from "@mui/material/Paper";
 import TaskCard from "../components/cards/TaskCard";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import PopupReusable from "../components/PopUp/PopupReusable";
 const Tasks = () => {
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{ backgroundColor: "lightgray", p: 4, height: "100vh" }}
-    >
-      <Grid size={12} px={1}>
-        <TextField
-          sx={{ backgroundColor: "white", overflow: "hidden", borderRadius: 1 }}
-          id="outlined-basic"
-          placeholder="Search"
-          size="small"
-          variant="outlined"
-          fullWidth
-        />
-        <Button variant="contained">Contained</Button>
-      </Grid>
+    <>
       <Grid
-        size={{ xs: 12, md: 2, lg: 3 }}
-        sx={{
-          px: 1,
-          height: "100% ",
-          overflowY: "scroll",
-        }}
+        container
+        spacing={2}
+        sx={{ backgroundColor: "lightgray", p: 4, height: "100vh" }}
       >
-        <Typography variant="h4" color="primary.main" mb={1} fontWeight={700}>
-          Tasks
-        </Typography>
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
+        <Grid size={12} px={1}>
+          <TextField
+            sx={{
+              backgroundColor: "white",
+              overflow: "hidden",
+              borderRadius: 1,
+            }}
+            id="outlined-basic"
+            placeholder="Search"
+            size="small"
+            variant="outlined"
+            fullWidth
+          />
+          <Button variant="contained">Contained</Button>
+        </Grid>
+        <Grid
+          size={{ xs: 12, md: 2, lg: 3 }}
+          sx={{
+            px: 1,
+            height: "100% ",
+            overflowY: "scroll",
+          }}
+        >
+          <Typography variant="h4" color="primary.main" mb={1} fontWeight={700}>
+            Tasks
+          </Typography>
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
+        </Grid>
+        <Grid size={{ xs: 12, md: 2, lg: 3 }}>
+          <Paper>2</Paper>
+        </Grid>
+        <Grid size={{ xs: 12, md: 2, lg: 3 }}>
+          <Paper>3</Paper>
+        </Grid>
+        <Grid size={{ xs: 12, md: 2, lg: 3 }}>
+          <Paper>4</Paper>
+        </Grid>
       </Grid>
-      <Grid size={{ xs: 12, md: 2, lg: 3 }}>
-        <Paper>2</Paper>
-      </Grid>
-      <Grid size={{ xs: 12, md: 2, lg: 3 }}>
-        <Paper>3</Paper>
-      </Grid>
-      <Grid size={{ xs: 12, md: 2, lg: 3 }}>
-        <Paper>4</Paper>
-      </Grid>
-    </Grid>
+      <PopupReusable />
+    </>
   );
 };
 
